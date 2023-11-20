c17a05a88e3ca9b8c7bd9b523058de56271745a6
Maruf Khan <marufatrai12@gmail.com>
2023-11-20 09:34:47 +0600

    JS-Class-14

---
+++ Javascript/script.js
@@ -0,0 +1,35 @@
+
+
+    //  Javascript if else Statement Assignment
+
+    var marks = prompt("Enter a Marks : ");
+
+    if(marks >= 80){
+
+        document.write("A+");
+
+    }else if(marks >= 70){
+
+        document.write("A");
+
+    }else if(marks >= 60){
+
+        document.write("A-");
+
+    }else if(marks >= 50){
+
+        document.write("B");
+
+    }else if(marks >= 40){
+
+        document.write("C");
+
+    }else if(marks >= 33){
+
+        document.write("D");
+
+    }else{
+
+        document.write("Faild!");
+
+    }
